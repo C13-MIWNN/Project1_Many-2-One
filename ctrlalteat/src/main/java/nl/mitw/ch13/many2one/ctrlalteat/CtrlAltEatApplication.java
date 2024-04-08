7bee5dad85d6a0da2f247c933f1d2de10cfe9a3a
Zarbolan <simonhiemstra1@gmail.com>
2024-04-08 14:33:28 +0200

    readded spring

---
+++ ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/CtrlAltEatApplication.java
@@ -0,0 +1,13 @@
+package nl.mitw.ch13.many2one.ctrlalteat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtrlAltEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtrlAltEatApplication.class, args);
+	}
+
+}
---
+++ ctrlalteat/src/test/java/nl/mitw/ch13/many2one/ctrlalteat/CtrlAltEatApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.mitw.ch13.many2one.ctrlalteat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtrlAltEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
