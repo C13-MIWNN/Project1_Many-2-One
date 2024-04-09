c139112c63f661aacdd14736a5a0470ff517305b
Linda Munsterman <l.munsterman@hotmail.com>
2024-04-09 12:15:01 +0200

    RecipeModel & recipeController

--- ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/controller/RecipeController.java
+++ ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/controller/RecipeController.java
@@ -1,8 +1,28 @@
 package nl.mitw.ch13.many2one.ctrlalteat.controller;
 
+import nl.mitw.ch13.many2one.ctrlalteat.repositories.RecipeRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
- * @author Simon Hiemstra
- * Purpose:
+ * @author Linda Munsterman
+ * Purpose: Handle all requestes regarding recipes
  **/
+
+@Controller
 public class RecipeController {
+    private final RecipeRepository recipeRepository;
+
+    public RecipeController(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @GetMapping({"/", "/recipe"})
+    private String showRecipeOverview(Model model) {
+
+        model.addAttribute("allRecipes", recipeRepository.findAll());
+
+        return "recipeOverview";
+    }
 }
--- ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/model/Recipe.java
+++ ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/model/Recipe.java
@@ -16,11 +16,57 @@ public class Recipe {
     @Id @GeneratedValue
     private Long recipeId;
     private String recipeName;
-    private  preparationTime;
-    private String recipeImage;
+    private int preparationTimeInMinutes;
+    private int servingSizeInPersons;
+    private String cuisine;
 
+    public Recipe(String recipeName, int preparationTimeInMinutes, int servingSizeInPersons, String cuisine) {
+        this.recipeName = recipeName;
+        this.preparationTimeInMinutes = preparationTimeInMinutes;
+        this.servingSizeInPersons = servingSizeInPersons;
+        this.cuisine = cuisine;
+    }
 
+    public Recipe() {
+    }
 
+    public Long getRecipeId() {
+        return recipeId;
+    }
 
+    public void setRecipeId(Long recipeId) {
+        this.recipeId = recipeId;
+    }
 
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public int getPreparationTimeInMinutes() {
+        return preparationTimeInMinutes;
+    }
+
+    public void setPreparationTimeInMinutes(int preparationTimeInMinutes) {
+        this.preparationTimeInMinutes = preparationTimeInMinutes;
+    }
+
+    public int getServingSizeInPersons() {
+        return servingSizeInPersons;
+    }
+
+    public void setServingSizeInPersons(int servingSizeInPersons) {
+        this.servingSizeInPersons = servingSizeInPersons;
+    }
+
+    public String getCuisine() {
+        return cuisine;
+    }
+
+    public void setCuisine(String cuisine) {
+        this.cuisine = cuisine;
+    }
 }
