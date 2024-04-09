e6efc4e74a9aac3969ea60dc000405cfc8926dfb
Arjan Cnossen <arjancnossenn@gmail.com>
2024-04-09 12:16:11 +0200

    Added dietary properties

---
+++ ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/controller/DietaryPropertyController.java
@@ -0,0 +1,17 @@
+package nl.mitw.ch13.many2one.ctrlalteat.controller;
+
+import nl.mitw.ch13.many2one.ctrlalteat.repositories.DietaryPropertyRepository;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Auteur Arjan Cnossen
+ * Handle everything related to dietary properties
+ **/
+@Controller
+public class DietaryPropertyController {
+    private final DietaryPropertyRepository dietaryPropertyRepository;
+
+    public DietaryPropertyController(DietaryPropertyRepository dietaryPropertyRepository) {
+        this.dietaryPropertyRepository = dietaryPropertyRepository;
+    }
+}
---
+++ ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/model/DietaryProperty.java
@@ -0,0 +1,32 @@
+package nl.mitw.ch13.many2one.ctrlalteat.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+/**
+ * @Auteur Arjan Cnossen
+ * Shows the dietary properties of an ingredient
+ **/
+public class DietaryProperty {
+
+    @Id @GeneratedValue
+    private Long DietaryPropertyId;
+
+    private String name;
+
+    public Long getDietaryPropertyId() {
+        return DietaryPropertyId;
+    }
+
+    public void setDietaryPropertyId(Long dietaryPropertyId) {
+        DietaryPropertyId = dietaryPropertyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
