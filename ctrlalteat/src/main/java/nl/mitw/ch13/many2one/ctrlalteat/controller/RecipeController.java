c6b89463e167250ca3f204b074aa1951b7d4c731
Linda Munsterman <l.munsterman@hotmail.com>
2024-04-10 14:32:53 +0200

    Recipe form. cannot save yet

--- ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/controller/RecipeController.java
+++ ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/controller/RecipeController.java
@@ -1,9 +1,13 @@
 package nl.mitw.ch13.many2one.ctrlalteat.controller;
 
+import nl.mitw.ch13.many2one.ctrlalteat.model.Recipe;
 import nl.mitw.ch13.many2one.ctrlalteat.repositories.RecipeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author Linda Munsterman
@@ -20,12 +24,22 @@ public class RecipeController {
 
     @GetMapping({"/", "/recipe"})
     private String showRecipeOverview(Model model) {
-
-
         model.addAttribute("allRecipes", recipeRepository.findAll());
-
         return "recipeOverview";
     }
 
+    @GetMapping("/recipe/new")
+    private String showRecipeForm(Model model) {
+        model.addAttribute("recipe", new Recipe());
+        return "recipeForm";
+    }
+
+    @PostMapping("/recipe/new")
+    private String saveRecipe(@ModelAttribute("recipe") Recipe recipeToBeSaved, BindingResult result) {
+        if (!result.hasErrors()) {
+            recipeRepository.save(recipeToBeSaved);
+        }
+        return "redirect:/";
+    }
 
 }
--- ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/repositories/RecipeRepository.java
+++ ctrlalteat/src/main/java/nl/mitw/ch13/many2one/ctrlalteat/repositories/RecipeRepository.java
@@ -3,9 +3,12 @@ package nl.mitw.ch13.many2one.ctrlalteat.repositories;
 import nl.mitw.ch13.many2one.ctrlalteat.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * @author Simon Hiemstra
  * Purpose:
  **/
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+Optional<Recipe> findByRecipeName(String recipeName);
 }
